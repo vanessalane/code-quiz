d7545a39c9b594614663a9a358b24822bd00f709
Vanessa Lane <vlane@23andme.com>
2020-06-14 15:15:42 -0700

    adds question object

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,49 @@ var timerElement = document.getElementById("timer");
 // define global variables
 var score = 0;
 var questionsAnswered = 0;
+var questions = [
+    {
+        question: "Which of the following is NOT a valid way to define a JS variable?",
+        answers: [
+            {answer: "var foo;", score: 0},
+            {answer: "var foo = bar;", score: 0},
+            {answer: "let foo = bar;", score: 0},
+            {answer: "foo = bar;", score: 5},
+        ]
+    },
+    {
+        question: "Which of the following is a valid way to define a JS function?",
+        answers: [
+            {answer: "function foo;", score: 5},
+            {answer: "var foo = bar() {console.log('something');}", score: 5},
+            {answer: "var foo = function() {console.log('something');}", score: 0},
+            {answer: "function() {console.log('something')}", score: 0},
+        ]
+    },
+    {
+        question: "What does JSON stand for?",
+        answers: [
+            {answer: "JavaScript Object Notation", score: 5},
+            {answer: "JavaScript Online Notes", score: 0},
+            {answer: "JavaScript Outline Network", score: 0},
+            {answer: "JSON does not stand for anything", score: 0},
+        ]
+    },
+    {
+        question: "True or False: JavaScript arrays are defined with square brackets ([]).",
+        answers: [
+            {answer: "True", score: 5},
+            {answer: "False", score: 0}
+        ]
+    },
+    {
+        question: "True or False: JavaScript object values must be strings.",
+        answers: [
+            {answer: "True", score: 0},
+            {answer: "False", score: 5}
+        ]
+    }
+]
 
 var startQuiz = function() {
     var timeLeft = 300000;
